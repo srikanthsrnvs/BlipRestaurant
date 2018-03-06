52c14d4f924e1324989337f4ab9f6491eb6d7aa5
Me <Srikanth@srikanths-imac-2.local>
2018-03-06 16:29:27 -0500

    initial homepage design skeleton

--- Blip/CustomClasses/UIViewExtensions.swift
+++ Blip/CustomClasses/UIViewExtensions.swift
@@ -24,3 +24,113 @@ extension UIView{
         self.clipsToBounds = true
     }
 }
+
+extension UINavigationController{
+    
+    func setColorToNavBar(color: UIColor){
+        
+        let image = UIImage.imageFromColor(color: color)
+        self.navigationBar.setBackgroundImage(image, for: .default)
+        self.navigationBar.isTranslucent = true
+    }
+}
+
+extension UIImage{
+    
+    static func imageFromColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        
+        // create a 1 by 1 pixel context
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+        
+    }
+}
+
+extension UIViewController{
+    
+    
+    func blend(from: UIColor, to: UIColor, percent: Double) -> UIColor {
+        var fR : CGFloat = 0.0
+        var fG : CGFloat = 0.0
+        var fB : CGFloat = 0.0
+        var tR : CGFloat = 0.0
+        var tG : CGFloat = 0.0
+        var tB : CGFloat = 0.0
+        
+        from.getRed(&fR, green: &fG, blue: &fB, alpha: nil)
+        to.getRed(&tR, green: &tG, blue: &tB, alpha: nil)
+        
+        let dR = tR - fR
+        let dG = tG - fG
+        let dB = tB - fB
+        
+        let rR = fR + dR * CGFloat(percent)
+        let rG = fG + dG * CGFloat(percent)
+        let rB = fB + dB * CGFloat(percent)
+        
+        return UIColor(red: rR, green: rG, blue: rB, alpha: 1.0)
+    }
+    
+    // Pass in the scroll percentage to get the appropriate color    
+    func scrollColor(percent: Double) -> UIColor {
+        var start = #colorLiteral(red: 0, green: 0.8495121598, blue: 0, alpha: 1)
+        var end = UIColor.clear
+        var perc = percent
+        if percent < 0.5 {
+            // If the scroll percentage is 0.0..<0.5 blend between yellow and green
+            start = UIColor.yellow
+            end = UIColor.green
+        } else {
+            // If the scroll percentage is 0.5..1.0 blend between green and blue
+            start = UIColor.green
+            end = UIColor.blue
+            perc -= 0.5
+        }
+        
+        return blend(from: start, to: end, percent: perc * 2.0)
+    }
+    
+    func setTitleForNavBar(title:String, subtitle:String, gesture: UIGestureRecognizer?) {
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: -2, width: 0, height: 0))
+        
+        titleLabel.backgroundColor = UIColor.clear
+        titleLabel.textColor = UIColor.white
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        titleLabel.text = title
+        titleLabel.sizeToFit()
+        
+        let subtitleLabel = UILabel(frame: CGRect(x: 0, y: 18, width: 0, height: 0))
+        subtitleLabel.backgroundColor = UIColor.clear
+        subtitleLabel.textColor = UIColor.white
+        subtitleLabel.font = UIFont.systemFont(ofSize: 10)
+        subtitleLabel.text = subtitle
+        subtitleLabel.sizeToFit()
+        
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: max(titleLabel.frame.size.width, subtitleLabel.frame.size.width), height: 30))
+        titleView.addSubview(titleLabel)
+        titleView.addSubview(subtitleLabel)
+        
+        let widthDiff = subtitleLabel.frame.size.width - titleLabel.frame.size.width
+        
+        if widthDiff < 0 {
+            let newX = widthDiff / 2
+            subtitleLabel.frame.origin.x = abs(newX)
+        } else {
+            let newX = widthDiff / 2
+            titleLabel.frame.origin.x = newX
+        }
+        
+        if let tap = gesture{
+            titleView.addGestureRecognizer(tap)
+        }
+        
+        self.navigationItem.titleView = titleView
+    }
+}
+
--- Blip/ViewControllers/HomePage.swift
+++ Blip/ViewControllers/HomePage.swift
@@ -7,17 +7,156 @@
 //
 
 import UIKit
+import Material
+import Hero
+import GooglePlaces
+import PopupDialog
+import SwiftIcons
 
-class HomePage: UIViewController {
+class HomePage: UIViewController, UIScrollViewDelegate {
 
+
+    @IBOutlet weak var alphaView: UIView!
+    @IBOutlet weak var storeBackground: UIImageView!
+    @IBOutlet weak var storeLogo: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var storeBackgroundToTop: NSLayoutConstraint!
+    
+    fileprivate var tileImages = [UIImage.init(named: "aubergine"), UIImage.init(named: "milk"), UIImage.init(named: "chips"), UIImage.init(named: "glass"), UIImage.init(named: "grain"), UIImage.init(named: "meat"), UIImage.init(named: "baguette"), UIImage.init(named: "toaster"), UIImage.init(named: "ice-cream"), UIImage.init(named: "pizza")]
+    fileprivate var tileStrings = [
+        "produce",
+        "Dairy",
+        "Chips & Snacks",
+        "Beverages",
+        "Spices & Pantry",
+        "Meat & Fish",
+        "Bakery & Deli",
+        "Cereals & Breakfast",
+        "Frozen foods",
+        "Microwave meals",
+        "Grains & Bulk",
+        "Household goods"]
+    var userAddress: String?
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        prepareLogo()
+        prepareNavigationBar()
+        handleLocations()
         // Do any additional setup after loading the view.
+    }
+    
+    func prepareLogo(){
+        
+        self.storeLogo.layer.cornerRadius = self.storeLogo.frame.size.width/2
+        self.storeLogo.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        // THIS IS FOR ANIMATION
+    }
+    
+    func prepareScrollView(){
+        
+        // DO THIS TO LOAD THE SCROLLVIEW WITH 10 TILES, 2 IN EACH ROW
+    }
+    
+    func prepareNavigationBar(){
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeDeliveryAddress))
+        let cartButton = IconButton()
+        cartButton.setIcon(icon: .googleMaterialDesign(.shoppingCart), color: UIColor.white, forState: .normal)
+        let cartBarButton = UIBarButtonItem(customView: cartButton)
+        self.navigationItem.rightBarButtonItem = cartBarButton
+        self.navigationController?.setColorToNavBar(color: UIColor.clear)
+        self.setTitleForNavBar(title: "Loading", subtitle: "Tap to change location", gesture: tap)
     }
 }
+
+extension HomePage: CLLocationManagerDelegate, GMSAutocompleteViewControllerDelegate{
+    
+    
+    func handleLocations(){
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if (error != nil){
+                print("Reverse geocoder failed with error" + (error?.localizedDescription)!)
+                return
+            }
+            if (placemarks?.count)! >= 0 {
+                self.locationManager.stopUpdatingLocation()
+                let pm = placemarks![0] as CLPlacemark
+                self.displayLocationInfo(placemark: pm)
+            }
+            else {
+                print("Problem with geocoder data")
+            }
+        }
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark) {
+        if placemark != nil {
+            let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeDeliveryAddress))
+            //stop updating location to save battery life
+            self.userAddress = "\(placemark.postalCode!)"
+            self.setTitleForNavBar(title: "Delivering to \(userAddress!)", subtitle: "Tap to change location", gesture: tap)
+        }
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        for component in place.addressComponents!{
+            if component.type == "postal_code"{
+                userAddress = "\(component.name)"
+                let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeDeliveryAddress))
+                self.setTitleForNavBar(title: "Delivering to \(userAddress!)", subtitle: "Tap to change location", gesture: tap)
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print(error)
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        let errorPopup = PopupDialog(title: "Error", message: "Could not retrive your location. Please enter a delivery location manually, alternatively turn on location services within settings")
+        self.present(errorPopup, animated: true, completion: nil)
+    }
+    
+    @objc func changeDeliveryAddress(){
+        
+        let placePickerController = GMSAutocompleteViewController()
+        placePickerController.delegate = self
+        present(placePickerController, animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
+
+
